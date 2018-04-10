93b32969c792c5d1260034ae1915fba96bc09141
YebrailAndres <yebrail_andrs@hotmail.com>
2018-04-10 11:05:52 -0500

    Adding text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 />
-        <p />
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!.</p>
       </div>
     );
   }
